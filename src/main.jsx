eb936a4c6c659000811e884f430780d242e4e57d
siddhantpd <prasadsiddhant0407@gmail.com>
2025-11-24 12:13:20 +0530

    Update BrowserRouter basename to use environment variable

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter,Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename = "/WBJEE_EXAM/">
+    <BrowserRouter basename = {import.meta.env.BASE_URL} >
       <Routes>
         <Route path="*" element={<App />} />
       </Routes>
